c25705b1a62fa293d3955a30e76fa9fb18f8183f
Bilal El Houdaigui <bilal.elhoudaigui@gmail.com>
2026-02-17 19:39:30 +0000

    :lipstick: improve light/dark theme icon

--- src/components/AraTopBar.js
+++ src/components/AraTopBar.js
@@ -3,19 +3,17 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import SwitchUI from '@mui/material/Switch';
+import IconButton from '@mui/material/IconButton';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { CustomThemeContext } from '../themes/CustomThemeProvider'
 
 export default function AraTopBar() {
   const { currentTheme, setTheme } = useContext(CustomThemeContext)
   const isDark = Boolean(currentTheme === 'dark')
 
-  const themeText = !isDark ? 'نهار' : 'ليل';
-
-  const handleThemeChange = (event) => {
-    const { checked } = event.target
-    if (checked) {
+  const handleThemeChange = () => {
+    if (!isDark) {
       setTheme('dark')
     } else {
       setTheme('normal')
@@ -30,10 +28,9 @@ export default function AraTopBar() {
             <Button color="inherit" href="/">الصفحة الرئيسية</Button>
             <Button color="inherit" href="/how-it-works">للفضوليين</Button>
           </Typography>
-          <FormControlLabel
-            control={<SwitchUI checked={isDark} onChange={handleThemeChange} />}
-            label={themeText}
-          />
+          <IconButton color="inherit" onClick={handleThemeChange} aria-label="toggle theme">
+            {isDark ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
